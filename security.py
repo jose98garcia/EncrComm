46a0f443c5d6bcea7731f19c946cca826de9f59d
Jose Garcia <garcia.jose804@gmail.com>
2019-02-19 15:11:47 -0600

    File created for project

---
+++ security.py
@@ -0,0 +1,12 @@
+# Author: Jose Garcia
+# Programming Assignment - Information Security
+# --------------------------------------------------------
+# Goal - implement encrypted communication between two parties,
+# and evaluates performance of AES and RSA. Implement authenticated
+# communications between parties and evaluate performance of HMAC
+# and RSA digital signature.
+
+
+print("Hello World")
+
+
